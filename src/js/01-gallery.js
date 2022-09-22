6465cb50cba0d449e51da50f335275459c982065
Alexey-Kushnir <al7787@gmail.com>
2022-09-22 15:25:45 +0300

    simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const imgMarkup = createGallaryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createGallaryItem(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
