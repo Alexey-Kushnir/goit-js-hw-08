23144ce81fd1ed133c66e871949c04ae2ed5f17e
Alexey-Kushnir <al7787@gmail.com>
2022-09-27 01:15:48 +0300

    vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+let parsedTime = JSON.parse(currentTime);
+
+if (parsedTime.seconds) {
+  player.setCurrentTime(parsedTime.seconds);
+}
+player.setCurrentTime(0);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
