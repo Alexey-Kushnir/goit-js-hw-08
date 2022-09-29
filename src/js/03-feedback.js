6af9d4b46c8602b43d76c09c7d110ed4323e2687
Alexey-Kushnir <al7787@gmail.com>
2022-09-29 14:32:26 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,42 +13,37 @@ form.addEventListener('input', throttle(onTextInput, 500));
 
 function onFormSubmit(e) {
   e.preventDefault();
-
   if (!message.value) {
     alert('Поле Message не заполнено');
     return;
   }
-
   if (!email.value) {
     alert('Поле Email не заполнено');
     return;
   }
-
   if (savedMessage) {
     console.log(savedMessage);
   } else {
     console.log(formData);
   }
-
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onTextInput(e) {
-  formData[e.target.name] = e.target.value;
+  formData[email.name] = email.value;
+  formData[message.name] = message.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateTextarea() {
   savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
   if (savedMessage) {
     if (savedMessage.email) {
       email.value = savedMessage.email;
     } else {
       email.value = '';
     }
-
     if (savedMessage.message) {
       message.value = savedMessage.message;
     } else {
